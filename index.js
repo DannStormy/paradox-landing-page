ff867029f31f88fde3cfdd756f5b9833a044b6b0
Daniel Isaac <dillydot22@gmail.com>
2022-08-18 20:36:55 +0100

    fixed hamburger

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $.noConflict();
 jQuery(document).ready(function($){
   $(".hamburger").click(() => {
-    $(".nav-items").slideToggle(1000);
+    $(".nav-items").slideToggle(500);
     let path = ($(".hamburger").attr("src") === "/assets/icons/icons8-menu.svg")
     ? "/assets/icons/close-icon.svg"
     : "/assets/icons/icons8-menu.svg";
